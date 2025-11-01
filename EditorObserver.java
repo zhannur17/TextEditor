012fc4503e2762e27fab2293e678bd117445c5b7
zhannur17 <zannurmejrambek87@gmail.com>
2025-11-01 15:45:24 +0500

    Refactor EditorObserver to EditorUIObserver
    Replaced EditorObserver class with EditorUIObserver class for UI-specific updates.

--- EditorObserver.java
+++ EditorObserver.java
@@ -1,12 +1,6 @@
-class EditorObserver implements Observer {
-    private String name;
-
-    public EditorObserver(String name) {
-        this.name = name;
-    }
-
+class EditorUIObserver implements Observer {
     @Override
     public void update(String message) {
-        System.out.println("[Observer " + name + "] " + message);
+        System.out.println("[UI] " + message);
     }
 }
